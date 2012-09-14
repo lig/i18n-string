39ee381a39b383faad06f3fdaf7e483fffbc7320
Serge Matveenko <s@matveenko.ru>
2012-09-14 16:14:46 +0400

    Stable. Version 0.1.

---
+++ i18n_string.py
@@ -0,0 +1,44 @@
+from collections import Mapping
+from locale import getdefaultlocale, normalize
+
+
+class LocaleDict(dict):
+
+    def __new__(cls, data=None):
+        self = dict.__new__(cls)
+
+        if data:
+
+            if not isinstance(data, Mapping):
+                raise ValueError(
+                    'Initial data must be instance of any mapping')
+
+            for k, v in data.items():
+                self[normalize(k)] = unicode(v)
+
+        return self
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __getitem__(self, key):
+        return super(LocaleDict, self).__getitem__(normalize(key))
+
+    def __setitem__(self, key, value):
+        return super(LocaleDict, self).__setitem__(normalize(key), value)
+
+
+class MultilingualString(unicode):
+
+    def __new__(cls, translations=None, default_language=None):
+        language = (default_language and normalize(default_language) or
+            '.'.join(getdefaultlocale()))
+        translations = LocaleDict(translations)
+        value = translations[language]
+        self = unicode.__new__(cls, value)
+        self.language = language
+        self.translations = translations
+        return self
+
+    def translate(self, language):
+        return self.__class__(self.translations, language)
---
+++ test.py
@@ -0,0 +1,41 @@
+# coding: u8
+import unittest
+
+from i18n_string import MultilingualString, LocaleDict
+
+
+class TestMultilingualString(unittest.TestCase):
+
+    def setUp(self):
+        self.s = MultilingualString({'en': 'Hermitage', 'ru': u'Эрмитаж'})
+
+    def test001_string_type(self):
+        self.assertIsInstance(self.s, MultilingualString)
+        s = self.s.translate('en')
+        self.assertIsInstance(s, MultilingualString)
+
+    def test002_translations_type(self):
+        self.assertIsInstance(self.s.translations, LocaleDict)
+        s = self.s.translate('en')
+        self.assertIsInstance(s.translations, LocaleDict)
+
+    def test003_translations_data(self):
+        self.assertDictEqual(
+            self.s.translations,
+            {'en_US.ISO8859-1': u'Hermitage', 'ru_RU.UTF-8': u'Эрмитаж'})
+
+    def test004_translate(self):
+        s = self.s.translate('en')
+        self.assertMultiLineEqual(s, u'Hermitage')
+        s = self.s.translate('ru')
+        self.assertMultiLineEqual(s, u'Эрмитаж')
+
+    def test005_translations_update(self):
+        self.s.translations['ru'] = u'Государственный Эрмитаж'
+        self.assertIsInstance(self.s.translations, LocaleDict)
+        self.assertMultiLineEqual(
+            self.s.translations['ru'], u'Государственный Эрмитаж')
+
+
+if __name__ == '__main__':
+    unittest.main()
